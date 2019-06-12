8358baa122a5719e83ca39ca401f7a07c8d448e2
fotcorn <fotcorn@gmail.com>
2019-06-12 21:07:05 +0200

    docs: config for read the docs

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,7 @@ project = 'interpret-segmentation'
 copyright = '2019, Fabio Anderegg'
 author = 'Fabio Anderegg'
 
+master_doc = 'contents'
 
 # -- General configuration ---------------------------------------------------
 
@@ -51,3 +52,5 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
+
+autodoc_mock_imports = ['torch', 'torchvision']
