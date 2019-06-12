3b2a9480434ce01f9ae947595cf2b5fa588ac1ca
fotcorn <fotcorn@gmail.com>
2019-06-12 21:28:48 +0200

    setup: add correct packages to include in wheel

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
-    packages=['interpret_segmentation'],
+    packages=['interpret_segmentation.hdm', 'interpret_segmentation.rise'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
