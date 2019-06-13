176268027736857c7bb0ab6abe85c16cbedd50a8
fotcorn <fotcorn@gmail.com>
2019-06-13 10:21:59 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="interpret_segmentation",
-    version="0.0.3",
+    version="1.0.0",
     author="Fabio Anderegg",
     author_email="andef4@bfh.ch",
     description="Interpreing image segmentation models",
